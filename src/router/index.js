615bf61f98b9df490ea828b596269a8ff5f9e792
jasond <jasondansie@gmail.com>
2025-03-23 12:21:43 +0200

    adds createWebHashHistory  for render.com routing fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory  } from 'vue-router';
 import DashBoard from '@/components/dashBoard.vue';
 import Agents from '@/components/agentsPage.vue';
 import AddAgent from '@/components/addAgent.vue';
@@ -53,7 +53,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory (),
   routes,
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
