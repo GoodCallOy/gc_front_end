cb483d33d37069297b4aec7848f08017da73c54d
jasond <jasondansie@gmail.com>
2025-03-23 11:41:53 +0200

    adds style to login page

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -1,9 +1,18 @@
 <template>
-    <div class="login-container">
-      <h2>Login</h2>
-      <GoogleLoginButton />
-    </div>
-  </template>
+  <div class="login-container">
+    <v-card
+    :class="['mx-2 my-2', 'cardStyle']"
+    :elevation="16"
+    >
+      <div class="title-box"></div>
+
+      <div class="login-button">
+        <GoogleLoginButton />
+      </div>
+    </v-card>  
+  </div>
+ 
+</template>
   
   <script setup>
   import GoogleLoginButton from "@/components/googleLoginButton.vue";
@@ -15,7 +24,43 @@
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    height: 50vh;
+    height: 85vh;
   }
+
+  .login-button {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 30vh;
+    width: 60%;
+  }
+  .cardStyle {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 50vh;
+    width: 35%;
+    padding: 20px;
+    text-align: center;
+  }
+  .title-box {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 60vh;
+    width: 100%;
+    background-image: url("@/assets/Back_v1.jpg");
+    background-size: cover;
+   
+  }
+  .full-width-card {
+  width: 70%;
+  margin: 0;
+  padding: 20px;
+  text-align: center;
+}
   </style>
   
