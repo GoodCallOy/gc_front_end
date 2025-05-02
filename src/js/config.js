1db020b419bb7d69beb61545124ebbfc00348032
Jason Dansie <jasondansie@gmail.com>
2025-05-02 10:52:31 +0300

    adds changes in config

--- src/js/config.js
+++ src/js/config.js
@@ -2,12 +2,12 @@ const ENV = process.env.NODE_ENV;
 
 const config = {
   development: {
-    frontEndURL: "http://localhost:8080",
+    frontEndURL: "http://localhost:8080/api/v1",
     backEndURL: "http://localhost:3030/api/v1",
   },
   production: {
-    frontEndURL: "https://goodcall-front-end.onrender.com",
-    backEndURL: "https://goodcall-back-end.onrender.com",
+    frontEndURL: "https://goodcall-front-end.onrender.com/api/v1",
+    backEndURL: "https://goodcall-back-end.onrender.com/api/v1",
     goodCallBackEndURL: "https://goodcall.fi/api/v1",
   }
 };
