2f6e3fcbb79d69977cd129713321f67f8638ac69
jasond <jasondansie@gmail.com>
2025-06-17 18:32:28 +0300

    fixed config of ssl in dev

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,20 @@
 const fs = require('fs');
 const { defineConfig } = require('@vue/cli-service');
 
+const isDev = process.env.NODE_ENV === 'development';
+
 module.exports = defineConfig({
   transpileDependencies: true,
 
-  devServer: {
-    https: {
-      key: fs.readFileSync('C:/Users/j_dan/server.key'),
-      cert: fs.readFileSync('C:/Users/j_dan/server.cert'),
-    },
-    port: 8080, // or your preferred port
-  },
+  devServer: isDev
+    ? {
+        https: {
+          key: fs.readFileSync('C:/Users/j_dan/server.key'),
+          cert: fs.readFileSync('C:/Users/j_dan/server.cert'),
+        },
+        port: 8080,
+      }
+    : {},
 
   pluginOptions: {
     vuetify: {
